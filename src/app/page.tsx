1983c6d42c68e4ff373f682d50f2abcf56d36056
JuanOchoa99 <38961797+JuanOchoa99@users.noreply.github.com>
2025-11-16 12:18:06 -0500

    Delete components perks, platform, timeline and upgrade (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Hero from "@/components/Home/Hero";
 import Work from "@/components/Home/work";
-import TimeLine from "@/components/Home/timeline";
 import Platform from "@/components/Home/platform";
 import Portfolio from "@/components/Home/portfolio";
-import Upgrade from "@/components/Home/upgrade";
-import Perks from "@/components/Home/perks";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Servus",
@@ -16,11 +13,8 @@ export default function Home() {
     <main>
       <Hero />
       <Work />
-      <TimeLine />
       <Platform />
       <Portfolio />
-      <Upgrade />
-      <Perks />
     </main>
   );
 }
